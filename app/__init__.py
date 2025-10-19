5883360a054dd4f1da737b12532e10ac9b2d213a
Giffoni Lopes <kgiffoni_@tuta.com>
2025-10-19 10:03:14 -0300

    quickfix

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from app.config import Config
+from config import Config
 from app.extensions import db, migrate
 from app.routes import register_blueprints
 from app.seed import init_cli
