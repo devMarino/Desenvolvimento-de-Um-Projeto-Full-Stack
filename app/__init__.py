3feae8f8180205f213f1ff56d8177664b9735e8e
Giffoni Lopes <kgiffoni_@tuta.com>
2025-10-19 09:56:53 -0300

    quickfix

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from app.config import Config
 from app.extensions import db, migrate
 from app.routes import register_blueprints
+from app.seed import init_cli
 
 def create_app():
     app = Flask(__name__)
@@ -11,5 +12,12 @@ def create_app():
     migrate.init_app(app, db)
 
     register_blueprints(app)
+
+    init_cli(app) # Inicializa os comandos personalizados
+    with app.app_context():
+        try:
+            db.create_all() # Cria a tabela do banco
+        except:
+            print("Erro, o banco já existe") # Caso ja tenha sido criado
 
     return app
